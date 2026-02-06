92c5c2e4903ae1369bfb5e830f1996a5418feb45
Shweta Bharati <shwetabharati039@gmail.com>
2026-02-06 23:43:07 +0530

    added dashbord files

--- src/App.js
+++ src/App.js
@@ -15,26 +15,18 @@ const App = () => {
             </button>
           </TooltipComponent>
         </div>
-         { activeMenu ? (
+        { activeMenu ? (
           <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
             Sidebar
           </div>
-         ) : (
-          <div className="w-0 dark:bg-secondary-dark-bg">
-            Sidebar 
-            </div>
-         )}
-         <div className={
-          `dark:bg-main-bg bg-main-bg-main-bg min-h-screen w-full ${ activeMenu ? 'md:ml-72' : 'flex-2'}`
-                   }>
-                    <div className={`dark:bg-main-bg bg-main-bg-main-bg  min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
-                  }>
-                 <div>
+        ) : (
+          <div >
+            Sidebar w-0
+          </div>
 
-                 </div >
-                    </div>
-
-         </div>
+        )}
+    
+        
       </div>
     </BrowserRouter>
    </div>
