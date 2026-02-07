f0f23e969b8465b7528efcb03117a82c2c892c4b
Shweta Bharati <shwetabharati039@gmail.com>
2026-02-07 23:26:20 +0530

    Added dashboad files

--- src/App.js
+++ src/App.js
@@ -20,11 +20,19 @@ const App = () => {
             Sidebar
           </div>
         ) : (
-          <div >
-            Sidebar w-0
+          <div className="w-0 dark:bg-secondary-dark-bg"  >
+            Sidebar 
           </div>
 
         )}
+        <div className={
+          `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'  }`
+      
+        }>
+      <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+       Navbar
+      </div>
+        </div>
     
         
       </div>
