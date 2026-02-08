135062b830fa23b05eab19649759af1f22cbe28d
Shweta Bharati <shwetabharati039@gmail.com>
2026-02-08 23:19:20 +0530

    Added dashboad files

--- src/App.js
+++ src/App.js
@@ -47,6 +47,16 @@ const App = () => {
              <Route path="/editor" element="Editor"/>
                <Route path="/calendar" element="Calendar"/>
                  <Route path="/color-picker" element="ColorPicker"/>
+                 {/* Charts */} 
+                  <Route path="/line" element="Line"/>
+                  <Route path="/area" element="Area"/>
+                  <Route path="/bar" element="Bar"/>
+                  <Route path="/pie" element="Pie"/>
+                     <Route path="/financial" element="Financial"/>
+                      <Route path="/color-mapping" element="ColorMapping"/>
+                       <Route path="/payrmid" element="Payrmid"/>
+                        <Route path="/stacked" element="Stacked"/>
+                  
           </Routes>
         </div>
         
