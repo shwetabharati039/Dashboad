b3a370256559d0e55c5bd195722d727c49a00513
Shweta Bharati <shwetabharati039@gmail.com>
2026-02-11 23:57:20 +0530

    Added dashboad files

--- src/pages/Charts/ColorMapping.jsx
+++ src/pages/Charts/ColorMapping.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const ColorMapping = () => {
+const Colormapping = () => {
   return (
     <div>ColorMapping</div>
   )
 }
 
-export default ColorMapping
+export default Colormapping
